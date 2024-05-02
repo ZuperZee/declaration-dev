1204c4ec8daa41ce3d05149694e3332d0c587513
ZuperZee <ole.kristian.kaaseth@gmail.com>
2024-05-02 22:11:56 +0200

    Rework code

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 const max_depth = 999;
 const entryFile = "./types/global.d.ts";
-const typeDefs = getAllTypeDefinitions(entryFile, max_depth);
-typeDefs.forEach((typeDef) => {
+const definitionFiles = getAllTypeDefinitions(entryFile, max_depth);
+definitionFiles.forEach((typeDef) => {
   console.log(typeDef);
 });
 
@@ -13,40 +13,44 @@ function getAllTypeDefinitions(entryFile, max_depth) {
   const program = ts.createProgram([rootFile], { allowJs: true });
   const checker = program.getTypeChecker();
   const visitedFiles = new Set();
-  const typeDefinitions = new Set();
+  const definitionFiles = new Set();
 
-  function visit(node, depth) {
-    if (ts.isImportDeclaration(node) && node.importClause) {
-      const symbol = checker.getSymbolAtLocation(node.moduleSpecifier);
-      if (symbol && symbol.declarations) {
-        symbol.declarations.forEach((declaration) => {
-          const sourceFile = declaration.getSourceFile();
-          const filePath = sourceFile.fileName;
-          if (filePath.endsWith(".d.ts") && !visitedFiles.has(filePath)) {
-            visitedFiles.add(filePath);
-            typeDefinitions.add(filePath);
-            exploreFile(sourceFile, depth + 1);
-          }
-        });
-      }
-    }
-
-    ts.forEachChild(node, (n) => visit(n, depth));
-  }
-
-  function exploreFile(sourceFile, depth) {
-    if (max_depth < depth) {
-      return;
-    }
-    ts.forEachChild(sourceFile, (n) => visit(n, depth + 1));
-  }
-
-  const sourceFiles = program.getSourceFiles();
-  sourceFiles.forEach((sourceFile) => {
-    if (sourceFile.fileName === rootFile) {
-      exploreFile(sourceFile, 0);
-    }
+  let queue = program.getSourceFiles().filter((sourceFile) => {
+    return sourceFile.fileName === rootFile;
   });
 
-  return typeDefinitions;
+  // Used to keep track of the depth
+  let nextQueue = [];
+  let currentDepth = 1;
+
+  while (queue.length > 0 || nextQueue.length > 0) {
+    if (queue.length === 0) {
+      if (currentDepth >= max_depth) {
+        break;
+      }
+      queue = nextQueue;
+      nextQueue = [];
+      currentDepth += 1;
+    }
+    const sourceFile = queue.shift();
+
+    ts.forEachChild(sourceFile, (node) => {
+      if (ts.isImportDeclaration(node) && node.importClause) {
+        const symbol = checker.getSymbolAtLocation(node.moduleSpecifier);
+        if (symbol && symbol.declarations) {
+          symbol.declarations.forEach((declaration) => {
+            const sourceFile = declaration.getSourceFile();
+            const filePath = sourceFile.fileName;
+            if (filePath.endsWith(".d.ts") && !visitedFiles.has(filePath)) {
+              visitedFiles.add(filePath);
+              definitionFiles.add(filePath);
+              nextQueue.push(sourceFile);
+            }
+          });
+        }
+      }
+    });
+  }
+
+  return definitionFiles;
 }
